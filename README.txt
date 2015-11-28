61c20200d0740d09fab6b4fb93684bf3e3049893
40053531 <40053531@live.napier.ac.uk>
2015-11-28 23:11:51 +0000

    FINAL COMMIT added the report, readme, libraries and a 404 page

---
+++ README.txt
@@ -0,0 +1,5 @@
+To run the RSSReader you need to:
+1. install py_bcrypt via using "py_bcrypt.egg" in src/
+2. install feedparser via using "setup.py" in src/feedparser/
+3. install SQLite3 via the TCE command "tce-load -wi sqlite3-bin.tcz"
+4. run "main.py" with python "python main.py"
--- src/main.py
+++ src/main.py
@@ -179,5 +179,9 @@ def signup():
       return redirect(url_for('root'))
   return render_template('signup.html', error=error)
 
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html')
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
